7b23600d3204caf033d81115a627bcd755c994d7
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-03-11 00:30:22 +0100

    use annotation with spring AOP to add and remove the correlation in MDC

--- src/main/java/com/jonathanfoucher/sharedmdcexample/services/ExampleService.java
+++ src/main/java/com/jonathanfoucher/sharedmdcexample/services/ExampleService.java
@@ -1,14 +1,13 @@
 package com.jonathanfoucher.sharedmdcexample.services;
 
+import com.jonathanfoucher.sharedmdcexample.common.annotations.CorrelationIdMdc;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @Slf4j
@@ -17,16 +16,16 @@ public class ExampleService {
     private final ThreadRunnerService threadRunnerService;
 
     @Scheduled(cron = "0 */1 * * * *")
+    @CorrelationIdMdc
     public void tasksExecution() {
-        MDC.put("correlation-id", UUID.randomUUID().toString());
         log.info("START");
 
         threadRunnerService.run(() -> someTask(3000, 1));
         log.info("Single task completed");
 
         List<Runnable> runnables = new ArrayList<>();
-        runnables.add(() -> someTask(3000, 2));
-        runnables.add(() -> someTask(2000, 3));
+        runnables.add(() -> someTask(3000, 3));
+        runnables.add(() -> someTask(2000, 2));
         runnables.add(() -> someTask(4000, 4));
         threadRunnerService.run(runnables);
         log.info("Multiple tasks completed");
