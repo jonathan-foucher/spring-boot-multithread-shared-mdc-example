23a0fea530e9d1accf2719ee556dd9c9e60d41ed
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-03-10 23:55:04 +0100

    init the project

---
+++ src/main/java/com/jonathanfoucher/sharedmdcexample/services/ThreadRunnerService.java
@@ -0,0 +1,52 @@
+package com.jonathanfoucher.sharedmdcexample.services;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.MDC;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Service
+public class ThreadRunnerService {
+    private static final int NUMBER_OF_THREADS = 2;
+    private ExecutorService executorService;
+
+    @PostConstruct
+    public void postConstruct() {
+        executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        executorService.shutdown();
+    }
+
+    public void run(@NonNull Runnable runnable) {
+        Map<String, String> contextMap = MDC.getCopyOfContextMap();
+        CompletableFuture.runAsync(() -> {
+            MDC.setContextMap(contextMap);
+            runnable.run();
+            MDC.clear();
+        }, executorService).join();
+    }
+
+    public void run(@NonNull List<Runnable> runnables) {
+        Map<String, String> contextMap = MDC.getCopyOfContextMap();
+
+        List<CompletableFuture<Void>> completableFutures = runnables.stream()
+                .map(runnable -> CompletableFuture.runAsync(() -> {
+                    MDC.setContextMap(contextMap);
+                    runnable.run();
+                    MDC.clear();
+                }, executorService))
+                .toList();
+
+        completableFutures.forEach(CompletableFuture::join);
+    }
+}
